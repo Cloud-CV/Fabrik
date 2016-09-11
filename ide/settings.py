376afb3523e7fa962734e66fccfb114d66532f14
DESHRAJ <deshrajdry@gmail.com>
2016-09-11 20:03:04 +0000

    uwsgi and Nginx configuration added along with minor change in settings file

--- ide/settings.py
+++ ide/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
