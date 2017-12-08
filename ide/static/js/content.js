93784070cbec813ebbe425f3286288f6d8a5fe68
utsavgarg <utsav1996@ymail.com>
2017-12-08 18:05:02 +0530

    Moved model zoo to a separate file

--- ide/static/js/content.js
+++ ide/static/js/content.js
@@ -7,8 +7,8 @@ import TopBar from './topBar';
 import Tabs from './tabs';
 import data from './data';
 import netLayout from './netLayout_vertical';
-import ModelElement from './modelElement';
 import Modal from 'react-modal';
+import ModelZoo from './modelZoo';
 
 const infoStyle = {
   content : {
@@ -677,49 +677,7 @@ class Content extends React.Component {
   }
   zooModal() {
     this.modalHeader = null;
-    this.modalContent = <div className="zoo-modal">
-                        <div className="centered-zoo-modal">
-                        <div className="zoo-modal-model">
-                        <h3 className="zoo-modal-text">Recognition</h3>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="alexnet">AlexNet</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="All_CNN">All CNN</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="vgg16">VGG 16</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="densenet">DenseNet</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="GoogleNet">GoogLeNet</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="resnet101">ResNet 101</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="keras" id="v3">Inception V3</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="Squeezenet">Squeezenet</ModelElement>
-                        </div>
-                        <div className="zoo-modal-model">
-                        <h3 className="zoo-modal-text">Detection</h3>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="fcn">FCN32 Pascal</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="yolo_net">YOLONet</ModelElement>
-                        </div>
-                        <div className="zoo-modal-model">
-                        <h3 className="zoo-modal-text">Retrieval</h3>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="siamese_mnist">MNIST Siamese</ModelElement>
-                        </div>
-                        <div className="zoo-modal-model">
-                        <h3 className="zoo-modal-text">Seq2Seq</h3>
-                        <ModelElement importNet={this.importNet} framework="keras" id="textGeneration">Text Generation</ModelElement>
-                        <br/>
-                        <ModelElement importNet={this.importNet} framework="keras" id="seq2seq_lang">Seq2Seq Translation</ModelElement>
-                        </div>
-                        <div className="zoo-modal-model">
-                        <h3 className="zoo-modal-text">Caption</h3>
-                        <ModelElement importNet={this.importNet} framework="caffe" id="CoCo_Caption">CoCo Caption</ModelElement>
-                        </div>
-                        </div>
-                        </div>
-
+    this.modalContent = <ModelZoo importNet={this.importNet}/>;
     this.openModal();
   }
   render() {
---
+++ ide/static/js/modelZoo.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ModelElement from './modelElement';
+
+class ModelZoo extends React.Component {
+  render() {
+    return (
+      <div className="zoo-modal">
+        <div className="centered-zoo-modal">
+          <div className="zoo-modal-model">
+            <h3 className="zoo-modal-text">Recognition</h3>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="alexnet">AlexNet</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="All_CNN">All CNN</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="vgg16">VGG 16</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="densenet">DenseNet</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="GoogleNet">GoogLeNet</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="resnet101">ResNet 101</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="keras" id="v3">Inception V3</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="Squeezenet">Squeezenet</ModelElement>
+          </div>
+          <div className="zoo-modal-model">
+            <h3 className="zoo-modal-text">Detection</h3>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="fcn">FCN32 Pascal</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="yolo_net">YOLONet</ModelElement>
+          </div>
+          <div className="zoo-modal-model">
+            <h3 className="zoo-modal-text">Retrieval</h3>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="siamese_mnist">MNIST Siamese</ModelElement>
+          </div>
+          <div className="zoo-modal-model">
+            <h3 className="zoo-modal-text">Seq2Seq</h3>
+              <ModelElement importNet={this.props.importNet} framework="keras" id="textGeneration">Text Generation</ModelElement>
+              <br/>
+              <ModelElement importNet={this.props.importNet} framework="keras" id="seq2seq_lang">Seq2Seq Translation</ModelElement>
+          </div>
+          <div className="zoo-modal-model">
+            <h3 className="zoo-modal-text">Caption</h3>
+              <ModelElement importNet={this.props.importNet} framework="caffe" id="CoCo_Caption">CoCo Caption</ModelElement>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ModelZoo.propTypes = {
+  importNet: React.PropTypes.func
+};
+
+export default ModelZoo;
