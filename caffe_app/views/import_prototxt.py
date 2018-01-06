6f2871b723db07e7475f08078db7724025fceaad
Adam Štafa <34075272+adamstafa@users.noreply.github.com>
2018-01-06 19:24:08 +0100

    Auto-upgrade old prototxt (#248)
    * auto convert old prototxt
    
    * pass unit test

--- caffe_app/views/import_prototxt.py
+++ caffe_app/views/import_prototxt.py
@@ -4,6 +4,8 @@ from django.conf import settings
 import os
 from caffe.proto import caffe_pb2
 from google.protobuf import text_format
+import tempfile
+import subprocess
 
 
 # ******Data Layers******
@@ -559,8 +561,23 @@ def import_prototxt(request):
                 return JsonResponse({'result': 'error',
                                      'error': 'No Prototxt model file found'})
         caffe_net = caffe_pb2.NetParameter()
+
+        # try to convert to new prototxt
         try:
-            text_format.Merge(prototxt.read(), caffe_net)
+            content = prototxt.read()
+            tempFile = tempfile.NamedTemporaryFile()
+            tempFile.write(content)
+            tempFile.seek(0)
+            subprocess.call("~/caffe/caffe/build/tools/upgrade_net_proto_text "
+                            + tempFile.name + " " + tempFile.name, shell=True)
+            tempFile.seek(0)
+            content = tempFile.read()
+            tempFile.close()
+        except Exception as ex:
+            return JsonResponse({'result': 'error', 'error': 'Invalid Prototxt\n'+str(ex)})
+
+        try:
+            text_format.Merge(content, caffe_net)
         except Exception as ex:
             return JsonResponse({'result': 'error', 'error': 'Invalid Prototxt\n'+str(ex)})
 
