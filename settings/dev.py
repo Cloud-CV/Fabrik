8ae02938313559015b16ba896f95d0072841c7e6
DESHRAJ <deshraj@gatech.edu>
2018-02-20 00:13:49 -0500

    Add minor fixes

--- settings/dev.py
+++ settings/dev.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'ide.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'ide/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
