0cede6b0e1f18df2db31a034a35a09f2ea5bd025
Ram81 <ramramrakhya81@gmail.com>
2018-05-01 14:13:37 +0530

    Docker: Improve Docker Configuration for development environment

--- ide/settings.py
+++ ide/settings.py
@@ -92,11 +92,11 @@ SITE_ID = 1
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fabrik',
-        'USER': 'admin',
-        'PASSWORD': 'fabrik',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ.get("POSTGRES_NAME", 'postgres'),
+        'USER': os.environ.get("POSTGRES_USER", 'postgres'),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'postgres'),
+        'HOST': os.environ.get("POSTGRES_HOST", 'db'),
+        'PORT': os.environ.get("POSTGRES_PORT", 5432),
     }
 }
 
