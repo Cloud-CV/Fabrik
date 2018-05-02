43c8f995afabe7724bdbbd09c248676a3424d270
Deshraj <deshraj@gatech.edu>
2018-05-02 04:04:19 -0400

    Minor improvements

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-import sys
-
 # TODO: Add support for test and production environment settings
 
 try:
-    from .dev import *  
+    from .dev import *
     print("Using Dev settings")
 except ImportError:
     pass
