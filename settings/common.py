6891b40293bb8bdbe997066b2ebabd47c7c640d6
Ram Ramrakhya <ramramrakhya81@gmail.com>
2018-06-14 14:01:08 +0530

    Add Celery Setup (#361)

--- settings/common.py
+++ settings/common.py
@@ -102,3 +102,11 @@ CHANNEL_LAYERS = {
         "ROUTING": "ide.routing.channel_routing",
     },
 }
+
+# celery settings
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
